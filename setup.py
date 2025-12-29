2c14342560ae438815dad43eec1e3bd7f01a79ff
Sujil S <sujil192110912@gmail.com>
2025-12-29 11:01:33 +0530

    Initialize project structure with packaging, configs, and dependencies

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sujil S",
+    author_email="sujil192110912@gmail.com",
+    packages=find_packages()
+)
